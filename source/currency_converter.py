88fa418bdf07275af3734c4cbfcfe4cd7d60ab0e
Michal_Vitamvas <michalvitamvas8@gmail.com>
2025-08-06 00:12:42 +0200

    add main.py, projekt works

--- source/currency_converter.py
+++ source/currency_converter.py
@@ -1,49 +1,10 @@
-import argparse
-from exchange_rates import get_exchange_rates
+from source.exchange_rates import ExchangeRateProvider
 
-parser = argparse.ArgumentParser(description="Converter between CZK and EUR")
-parser.add_argument("-a", "--amount", type=float, help="Amount to be converted")
-parser.add_argument(
-    "-d",
-    "--direction",
-    type=str.lower,
-    choices=["from", "to"],
-    help="From / To",
-)
-parser.add_argument(
-    "-c",
-    "--currency",
-    type=str.lower,
-    help="Currency to be converted from/to",
-)
-parser.add_argument(
-    "-l", "--list", action="store_true", help="List all supported currencies"
-)
 
-args = parser.parse_args()
-rates = get_exchange_rates()
+class CurrencyConverter:
+    def __init__(self, rate_provider: ExchangeRateProvider) -> None:
+        self.rate_provider: ExchangeRateProvider = rate_provider
 
-if rates is None:
-    print("Failed to fetch exchange rates. Please check your network connection.")
-    exit(1)
-
-if args.list:
-    for rate in rates:
-        print(f"{rate[1]} {rate[3]}")
-    exit()
-
-for rate in rates:
-    if rate[1].lower() == args.currency or rate[3].lower() == args.currency:
-        if args.direction == "from":
-            res = args.amount * float(rate[4].replace(",", ".")) / float(rate[2])
-            print(f"Converted value: {res:.2f} {rate[3]}")
-            exit()
-        elif args.direction == "to":
-            res = args.amount / float(rate[4].replace(",", ".")) * float(rate[2])
-            print(f"Converted value: {res}")
-            exit()
-        else:
-            print("Invalid input")
-            exit()
-
-print("Currency was not found")
+    def convert(self, amount: float, from_currency: str, to_currency: str) -> float:
+        rate: float = self.rate_provider.get_rate(from_currency, to_currency)
+        return amount * rate

62d75b475ef2957cf415b7b4bc1590291bf4a1b6
Michal_Vitamvas <michalvitamvas8@gmail.com>
2025-08-08 01:42:51 +0200

    add first version of currency converter

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from source.currency_converter import CurrencyConverter
 from source.exchange_rates import ExchangeRateProvider
 
 
-if __name__ == "__main__":
+def main(amount: float, from_currency: str, to_currency: str) -> float:
     logging.basicConfig(level=logging.INFO)
     provider = ExchangeRateProvider(
         "https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt"
     )
     converter = CurrencyConverter(provider)
-    print(converter.convert(100, "EUR", "CZK"))
-    print(converter.convert(100, "CZK", "EUR"))
-    print(converter.convert(100, "EUR", "EUR"))
-    print(converter.convert(100, "EUR", "JPY"))
+    return converter.convert(amount, from_currency, to_currency)
